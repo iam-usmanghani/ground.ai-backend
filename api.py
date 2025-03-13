6a75fd534356f893b06b6da8c61be0ef63d9ba6a
GroundZero <officialahmed194@gmail.com>
2025-03-14 03:28:57 +0500

    Update api.py

--- api.py
+++ api.py
@@ -4,7 +4,6 @@ from flask_jwt_extended import JWTManager
 from Auth.auth import auth_bp
 from Models.models import db
 from Config.config import configurations
-from flask_cors import CORS
 import os
 from flask_cors import CORS
 def create_app():
@@ -16,7 +15,7 @@ def create_app():
     db.init_app(app)
     Bcrypt(app)
     JWTManager(app)
-
+    CORS(app, resources={r"/*": {"origins": "*"}})  
     app.register_blueprint(auth_bp)
 
     with app.app_context():
